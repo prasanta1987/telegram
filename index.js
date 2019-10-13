098635f469c177dd5a285824a10efe88e0478506
Prasanta Karmakar <prasanta.1987@hotmail.com>
2019-10-14 00:04:07 +0530

    1st comit

---
+++ index.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '759487040:AAG_7xUVOtC83Ru6z6kG2AMr4cT2ZasbLAE';
+const bot = new TelegramBot(token, { polling: true });
+
+const CHAT_ID = '338073979'
+
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+
+//     bot.sendMessage(chatId, 'Received your message');
+// });
+
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+
+//     const chatId = msg.chat.id;
+//     const resp = match[1];
+
+//     bot.sendMessage(chatId, resp);
+// });
+
+
+bot.addListener('text', (msg, resp) => {
+    console.log(msg.text)
+    const myChatId = msg.chat.id
+
+    bot.sendMessage(myChatId, `Hello ${msg.from.first_name} ${msg.from.last_name}, you have entered ${msg.text}`)
+})
+
+
+// bot.on('message', (msg) => {
+//     console.log(msg)
+// })
